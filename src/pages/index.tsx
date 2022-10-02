262aa2d2c238d2cefba052d282ea3c77c7639fbe
FanYuang <1155124596@link.cuhk.edu.hk>
2022-10-02 13:14:44 -0500

    11

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import dynamic from 'next/dynamic';
 import {FC, memo} from 'react';
 import Page from '../components/Layout/Page';
 import About from '../components/Sections/About';
@@ -7,7 +6,7 @@ import Resume from '../components/Sections/Resume';
 import {homePageMeta} from '../data/data';
 
 // eslint-disable-next-line react-memo/require-memo
-const Header = dynamic(() => import('../components/Sections/Header'), {ssr: false});
+
 
 const Home: FC = memo(() => {
   const {title, description} = homePageMeta;
